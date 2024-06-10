b73d0218b2b32fdbe9e9fbb4629dca5f97429202
Nickoprogrammer <nickoprogrammer@gmail.com>
2024-06-11 00:03:13 +0300

    Initial commit

---
+++ Courses/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Item
+from .serializers import ItemSerializer
+
+
+class ItemListCreate(generics.ListCreateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+class ItemRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+
+def homepage(request):
+    return render(request, "index.html")
